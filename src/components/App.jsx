1ab72ea12417a90ea327bda04fd8b416f2687abd
Marko Kustudija <markokustudija@Markos-MacBook-Air.local>
2021-05-20 18:20:39 +0200

    adding CreateArea and Note.js, editing styles.css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import CreateArea from "./CreateArea";
+import Note from "./Note";
 
 
 function App (){
     return(
         <div>
         <Header/>
+        <CreateArea />
+        <Note key = {1} title = "Note title" content = "Note content"/>
         <Footer/>
         </div>
         
