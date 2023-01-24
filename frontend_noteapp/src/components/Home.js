88b8631661e33bf1adc37c30eb226b9a4a0e73b1
marko <marko@marko.com>
2023-01-24 11:40:38 +0100

    adding UI

--- frontend_noteapp/src/components/Home.js
+++ frontend_noteapp/src/components/Home.js
@@ -1,10 +1,16 @@
+import Card from "./UI/Card";
+
 const Home = () => {
     return (
-        <div > 
+        <Card > 
 
              <h1> Welcome to Home Page</h1>
+             
+             <p>This is the Note App where you can add your notes...</p>
+             <p>If you want to manage your notes, you can delete them and add the new ones...</p>
+            
 
-        </div>
+        </Card>
       );
 }
  
